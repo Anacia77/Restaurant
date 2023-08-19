9badda9752fe7609946b7e9c06d9839cb5e78988
Anacia77 <edemdoamekpor7@gmail.com>
2023-08-19 17:29:58 +0000

    mysql setup

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -77,10 +77,19 @@ WSGI_APPLICATION = 'restaurant.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.mysql',
+        #'NAME': BASE_DIR / 'db.mysql',
+        'NAME': 'restaurant',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': 'aiCanA'
+
     }
 }
+    
+
 
 
 # Password validation
